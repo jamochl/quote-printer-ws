8ac216e51957bd2b75d187f2e7920abc58a68907
James C. H. Lim <james.lim.ori@gmail.com>
2020-10-14 15:39:50 +1100

    base css, template and quote grabbing done. Should be ready to work

---
+++ quoteList.js
@@ -0,0 +1,20 @@
+let quoteList = {
+        "quotes": [
+        {
+            "body": "Test1",
+            "author": "Test1"
+        },
+        {
+            "body": "Test2",
+            "author": "Test2"
+        },
+        {
+            "body": "Test3",
+            "author": "Test3"
+        },
+        {
+            "body": "Test4",
+            "author": "Test4"
+        },
+    ]
+}
