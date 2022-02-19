5daa02efa4e97ed488c17462470efe3f079763f3
jamochl <james.lim@jamochl.com>
2022-02-20 09:35:25 +1100

    Fix several bugs
    * InnerHTML += breaks existing pages, changed to use
      document.createElement() and document.appendChild()
    * Fix CSS to avoid floats and use flexbox
    * Make Remove Quote part of each page, allowing you to delete specific
      quotes

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-function removeQuote() {
-    const quoteList = document.getElementsByClassName("page");
-    const listLength = quoteList.length
-    if (listLength > 1) {
-        quoteList[listLength - 1].remove();
+function createPage() {
+    const pageDOM = document.createElement("div");
+    pageDOM.setAttribute("class", "page quote-section__page");
+
+    const boxDOM = document.createElement("div");
+    boxDOM.setAttribute("class", "box page__box")
+
+    const textareaDOM = document.createElement("textarea");
+    textareaDOM.setAttribute("class", "box__quote-body textarea no-print");
+    textareaDOM.setAttribute("placeholder", "Quote Body");
+    textareaDOM.setAttribute("rows", "4");
+    const inputDOM = document.createElement("input");
+    inputDOM.setAttribute("class", "box__author text-input no-print");
+    inputDOM.setAttribute("placeholder", "author");
+    const quoteBodyDOM = document.createElement("p");
+    quoteBodyDOM.setAttribute("class", "box__quote-body para");
+    const authorDOM = document.createElement("p");
+    authorDOM.setAttribute("class", "box__author para");
+
+    const removeQuoteButtonDOM = document.createElement("button");
+    removeQuoteButtonDOM.setAttribute("class", "button page__button no-print");
+    removeQuoteButtonDOM.textContent = "Remove Quote"
+
+    pageDOM.appendChild(boxDOM);
+    boxDOM.appendChild(textareaDOM);
+    boxDOM.appendChild(inputDOM);
+    boxDOM.appendChild(quoteBodyDOM);
+    boxDOM.appendChild(authorDOM);
+    pageDOM.appendChild(removeQuoteButtonDOM);
+
+    const updateBodyPara = () => {
+        quoteBodyDOM.textContent = textareaDOM.value;
     }
+    textareaDOM.addEventListener("input", updateBodyPara)
+
+    const updateAuthorPara = () => {
+        authorDOM.textContent = inputDOM.value;
+    }
+    inputDOM.addEventListener("input", updateAuthorPara)
+
+    const removeQuote = () => {
+        pageDOM.remove();
+    }
+    removeQuoteButtonDOM.addEventListener("click", removeQuote);
+
+    return pageDOM;
 }
 
 function addQuote() {
-    const quoteSection = document.getElementById("quote-section")
-    quoteSection.innerHTML +=
-    `<div class="page">
-        <div class="box page__box">
-            <textarea class="box__quote-body textarea no-print" placeholder="Quote Body"></textarea> 
-            <input type="text" class="box__author text-input no-print" placeholder="author"></input>
-            <p class="box__quote-body para no-screen"></p> 
-            <p class="box__author para no-screen"></p>
-        </div>
-    </div>`
-
-    const textAreas = document.getElementsByClassName("box__quote-body textarea");
-    const quoteParas = document.getElementsByClassName("box__quote-body para");
-    const textArea = textAreas[textAreas.length - 1]
-    const quotePara = quoteParas[quoteParas.length - 1] 
-    const updateBodyPara = () => {
-        quotePara.textContent = textArea.value;
-    }
-    textArea.addEventListener("input", updateBodyPara)
-
-    const textInputs = document.getElementsByClassName("box__author text-input");
-    const authorParas = document.getElementsByClassName("box__author para");
-    const textInput = textInputs[textInputs.length - 1]
-    const authorPara = authorParas[authorParas.length - 1] 
-    const updateAuthorPara = () => {
-        authorPara.textContent = textInput.value;
-    }
-    textInput.addEventListener("input", updateAuthorPara)
+    const quoteSectionDOM = document.getElementById("quote-section");
+    quoteSectionDOM.appendChild(createPage());
 }
 
 function main() {
     const addQuoteButton = document.getElementById("quote-add-button")
     addQuoteButton.addEventListener("click", addQuote);
-    const removeQuoteButton = document.getElementById("quote-remove-button")
-    removeQuoteButton.addEventListener("click", removeQuote);
     addQuote();
 }
 
