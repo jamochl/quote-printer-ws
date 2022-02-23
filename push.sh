abb23748ba53a5775774d04fc077934cb843ebbf
jamochl <james.lim@jamochl.com>
2022-02-23 23:34:07 +1100

    fix push script

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync --chown www-data:www-data -rv --delete css/ *.html *.js root@jamochl.com:/var/www/quote-printer
+rsync --chown www-data:www-data -rv --delete css *.html *.js root@jamochl.com:/var/www/quote-printer
 
