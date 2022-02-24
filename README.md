412b54bf47a317b928f9c268284d920f6c8469a9
jamochl <james.lim@jamochl.com>
2022-02-24 20:58:07 +1100

    Add del npm package, make gulp more like example

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Basic javascript website to print awesome A4 size quotes
 
-* No Dependencies
+* No Dependencies when running
 * Easy to host and deploy
 * Nice results!
 
 ## Developing
 
 ```
-npm install -g browser-sync
-browser-sync . -w --index=index.html
+npm ci
+npm run gulp
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,51 @@ const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const uglify = require("gulp-uglify");
 const browserSync = require("browser-sync").create();
+const del = require("del");
 
-exports.default = async () => {
+exports.clean = async () => {
+    return del(['public']);
+}
+
+exports.parseCSS = async () => {
+    return gulp.src("css/*.css")
+    .pipe(postcss())
+    .pipe(gulp.dest('./public/css'));
+}
+
+exports.parseJS = async () => {
+    return gulp.src("javascript/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'));
+}
+
+exports.parseHTML = async () => {
+    return gulp.src("html/*.html")
+    .pipe(gulp.dest('./public'))
+}
+
+exports.parseAll = gulp.parallel(
+            exports.parseCSS,
+            exports.parseHTML,
+            exports.parseJS
+        );
+
+exports.watch = async () => {
     browserSync.init({
         server: {
             baseDir: "./public",
             index: "index.html"
         }
     });
-
-    const parseCSS = () => {
-        return gulp.src("css/*.css")
-        .pipe(postcss())
-        .pipe(gulp.dest('./public/css'));
-    }
-    const parseJS = () => {
-        return gulp.src("javascript/*.js")
-        .pipe(uglify())
-        .pipe(gulp.dest('./public'));
-    }
-    const parseHTML = () => {
-        return gulp.src("html/*.html")
-        .pipe(gulp.dest('./public'))
-    }
     const reloadBrowser = async () => {
         browserSync.reload();
     }
-    gulp.watch("css/*.css", { ignoreInitial: false }, gulp.series(parseCSS, reloadBrowser));
-    gulp.watch("javascript/*.js", { ignoreInitial: false }, gulp.series(parseJS, reloadBrowser));
-    gulp.watch("html/*.html", { ignoreInitial: false }, gulp.series(parseHTML, reloadBrowser));
+    // Reload browser once to get all new files in public
+    reloadBrowser();
+
+    gulp.watch("css/*.css", gulp.series(exports.parseCSS, reloadBrowser));
+    gulp.watch("javascript/*.js", gulp.series(exports.parseJS, reloadBrowser));
+    gulp.watch("html/*.html", gulp.series(exports.parseHTML, reloadBrowser));
 }
+
+exports.default = gulp.series(exports.clean, exports.parseAll, exports.watch)
